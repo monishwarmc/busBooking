f21f36bcf33a1ada848a6070cb3eb9313dfbef53
monishwarmc <monishwar69@gmail.com>
2024-03-05 00:26:09 +0530

    changes

--- booking.php
+++ booking.php
@@ -1,28 +1,30 @@
 <?php
-$server="127.0.0.1";
-$username="root";
-$password="";
-$database="booking";
-$conn=new mysql($server,$username,$password,$database);
-if(conn->connect_error){
-    die("Connection failed : ",$conn->connect_error);
+$server = "127.0.0.1";
+$username = "root";
+$password = "";
+$database = "booking";
+$conn = new mysqli($server, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-$name=$_POST['fname'];
-$number=$_POST['phone'];
-$ddate=date('Y-m-d',strtotime($_POST['ddate']));
-$sperson=$_POST['person'];
-$lname=$_POST['lname'];
-$eid=$_POST['email'];
-$adtae=date('Y-m-d',strtotime($_POST['adate']));
-$sbus=$_POST['bus'];
 
-$sql="insert into data values('$fname','$number''$ddate','$guest','$lname',''$eid','$adate','$sbus')";
+$name = $_POST['fname'];
+$number = $_POST['phone'];
+$ddate = date('Y-m-d', strtotime($_POST['ddate']));
+$sperson = $_POST['person'];
+$lname = $_POST['lname'];
+$eid = $_POST['email'];
+$adate = date('Y-m-d', strtotime($_POST['adate']));
+$sbus = $_POST['bus'];
 
-if(mysql_query($conn,$sql)){
-    echo "<script>alert("Booking Successfully");</script>";
+$sql = "INSERT INTO data (fname, phone, ddate, guest, lname, email, adate, bus) 
+        VALUES ('$name', '$number', '$ddate', '$sperson', '$lname', '$eid', '$adate', '$sbus')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "<script>alert('Booking Successfully');</script>";
+} else {
+    echo "<script>alert('Error: " . $sql . "<br>" . $conn->error . "');</script>";
 }
-else
-{
-    echo "<script>alert("Try again");</script>";
-}
+
+$conn->close();
 ?>
